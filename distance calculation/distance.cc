dea77b5a28d3649f1126202415f7d112575dc1a6
C00293259 <C00293259@louisiana.edu>
2021-10-24 00:30:14 -0500

    contd.

--- distance calculation/distance.cc
+++ distance calculation/distance.cc
@@ -14,8 +14,7 @@ double cal_dis(double speed, double time){
 
 int main(){
     double speed = 1.128609; //speed of sound in air in feet per millisecond
-    srand (time(NULL)); //intialize random seed
-    double incSinceTimeSent = 8248; //variable changes for testing purposes
+    double incSinceTimeSent = 4349; //variable changes for testing purposes
     printf("\n The calculated Distance(ft) : %f", cal_dis(speed,incSinceTimeSent));
 
     return 0;
